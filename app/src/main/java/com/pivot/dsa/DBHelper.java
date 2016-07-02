7abee8d5dc67998c58b119db061aeb43840f4a4a
iamnandans <iamnandans@gmail.com>
2016-07-02 15:34:02 +0530

    bug fix

--- app/src/main/java/com/pivot/dsa/DBHelper.java
+++ app/src/main/java/com/pivot/dsa/DBHelper.java
@@ -13,7 +13,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     private Context context;
     private static final String DB_NAME = "DSA";
-    private static final int DB_VERSION = 35;
+    private static final int DB_VERSION = 38;
     private SQLiteDatabase gDB;
     DBSubjects subjects ;
     DBChapters chapters ;
